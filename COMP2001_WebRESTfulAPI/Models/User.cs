4542a0de679ee8f50c50d307cc4a612dea9f6f94
tom <thomas.gardner-1@studnets.plymouth.ac.uk>
2021-02-01 14:55:35 +0000

    Add project files.

---
+++ COMP2001_WebRESTfulAPI/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace COMP2001_WebRESTfulAPI.Models
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string CurrentPassword { get; set; }
+    }
+}
